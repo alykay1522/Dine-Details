b8007b9cf62a002f48e64df3c9f12be202654d96
alykay1522 <alyshameade.1522@gmail.com>
2026-06-02 00:10:23 -0500

    Update specials.js

--- utils/specials.js
+++ utils/specials.js
@@ -5,7 +5,7 @@ const PUBLIC_ID = "thislilpiggy/specials.json";
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    api_secret: process.env.CLOUDINARY_SECRET,
 });
 
 // ------------------------------
